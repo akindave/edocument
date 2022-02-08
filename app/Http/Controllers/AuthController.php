49dde7a63b7a08248f7e3eab0187f8078157c821
unknown <akintandavid8@gmail.com>
2022-02-08 18:50:03 +0100

    latest release

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+
+        // Check if field is empty
+        if (empty($email) or empty($password)) {
+            return response()->json(['status' => 'errr', 'message' => 'You must fill all the fields']);
+        }
+
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+     protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        // $name = $request->name;
+        $email = $request->email;
+        $user_type = $request->user_type;
+        $registration_id = $request->registration_id;
+
+        // Check if field is empty
+        if (empty($email) or empty($registration_id)) {
+            return response()->json(['status' => '204', 'message' => 'You must fill all the fields']);
+        }
+
+        // Check if email is valid
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return response()->json(['status' => '206', 'message' => 'You must enter a valid email']);
+        }
+
+       
+
+        // Check if user already exist
+        if (User::where('email', '=', $email)->exists()) {
+            return response()->json(['status' => 'error', 'message' => 'User already exists with this email']);
+        }
+
+        if($user_type == "student"){
+            if (User::where('matric_no', '=', $registration_id)->exists()) {
+                return response()->json(['status' => 'error', 'message' => 'User already exists with this Matric No']);
+            }
+        }elseif($user_type  == "staff"){
+            if (User::where('staff_id', '=', $registration_id)->exists()) {
+                return response()->json(['status' => 'error', 'message' => 'User already exists with this Staff ID']);
+            }
+        }
+       
+
+        // Create new user
+        try {
+            $user = new User();
+            // $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = app('hash')->make($request->registration_id);
+
+            if($user_type == "student"){
+
+                $user->matric_no = $request->registration_id;
+
+            }elseif($user_type  == "staff"){
+
+                $user->staff_id = $request->registration_id;
+
+            }
+           
+
+            if ($user->save()) {
+                //adding value to a reuest object
+                $reg = ['password' => $registration_id];
+                $request->merge($reg);
+                
+                return $this->login($request);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    protected function whoLoggedIn()
+    {
+
+    }
+
+
+}
---
+++ database/migrations/2022_02_08_125643_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->longText('password')->nullable();
+            $table->string('matric_no')->nullable();
+            $table->string('staff_id')->nullable();
+            $table->longText('picture')->nullable();
+            $table->string('storage')->nullable();
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
